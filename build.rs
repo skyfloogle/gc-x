0596578c2ed07155a1f8b1a1c6a21bc4fcc4d354
Floogle <18466542+skyfloogle@users.noreply.github.com>
2021-06-26 14:18:46 +0200

    new icon

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut res = winres::WindowsResource::new();
-    // placeholder icon
-    // https://iconarchive.com/show/gentle-edges-icons-by-pixelkit/Game-Controller-icon.html
     res.set_icon_with_id("assets/icon.ico", "icon");
     res.compile()?;
 
