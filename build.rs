37de7a8fc4fb0e75326f9b72fed0a342f97d0496
Floogle <18466542+skyfloogle@users.noreply.github.com>
2021-08-04 17:31:00 +0200

    capitalize name in winres

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut res = winres::WindowsResource::new();
-    res.set_icon_with_id("assets/icon.ico", "icon");
+    res.set_icon_with_id("assets/icon.ico", "icon").set("ProductName", "GC-X");
     res.compile()?;
 
     Ok(())
